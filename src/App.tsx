edecae280e04603a1cb5beea5b816b418331965a
Selcuk Kekec <khskekec@gmail.com>
2022-04-02 12:35:12 +0200

    eslint settings adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
+import { PhotoCamera } from '@mui/icons-material';
+import { Button, IconButton } from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
-import {store} from "./core/redux/store";
+import { store } from './core/redux/store';
 
-function App() {
-    return (
-        <Provider store={store}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        </Provider>
-    );
-}
-
-export default App;
+export const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <Button variant="contained" color="secondary">
+        ASD
+      </Button>
+      <IconButton
+        color="primary"
+        aria-label="upload picture"
+        component="span"
+        size="small"
+      >
+        <PhotoCamera />
+      </IconButton>
+    </div>
+  </Provider>
+);
