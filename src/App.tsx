1ff9b1f91af263e8121a6231e1af8cf55c9798ff
Selcuk Kekec <khskekec@gmail.com>
2022-04-04 23:31:45 +0200

    chore: theme folder moved to core

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 import ScrollToTop from './components/ScrollToTop';
+import ThemeConfig from './core/theme';
+import GlobalStyles from './core/theme/globalStyles';
 import Router from './routes';
-import ThemeConfig from './theme';
-import GlobalStyles from './theme/globalStyles';
 
 export const App = () => (
   <ThemeConfig>
