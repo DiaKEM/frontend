2932fff7e00a0c9abe1fc7fdc0c60b4a1b39c664
Selcuk Kekec <khskekec@gmail.com>
2022-04-04 22:20:28 +0200

    chore: authentication integrated as sandbox in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import { gql, useQuery } from '@apollo/client';
+import { Button } from '@mui/material';
 import React from 'react';
 import './App.css';
-import { getEnvironment, getReactEnvVar } from './core/utils/environment';
+import { useAppDispatch, useAppSelector } from './core/redux/hook';
+import {
+  getEnvironment,
+  getReactEnvVar,
+} from './core/utils/helper/environment';
+import { authSlice, base$ } from './page/Auth/auth.slice';
+import { LoginPage } from './page/Auth/LoginPage';
 
-const GET_ALL_USER = gql`
-  query GetUser {
-    userMany {
+const GET_USER_BY_ID = gql`
+  query GetUser($id: MongoID!) {
+    userById(_id: $id) {
       name
+      username
     }
   }
 `;
+
 const Test = () => {
-  const { loading, error, data } = useQuery(GET_ALL_USER);
+  const { loading, error, data } = useQuery(GET_USER_BY_ID, {
+    variables: { id: '620be585970c2ca0a3be1a2e' },
+  });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error :( {JSON.stringify(error)}</p>;
 
   return <pre>{JSON.stringify(data)}</pre>;
 };
-
+const useAuthentication = () => useAppSelector(base$);
 export const App = () => {
+  // const isAuthenticated = useAppSelector(state => state.auth.authenticated);
+  const { authenticated: isAuthenticated } = useAuthentication();
   const isInstalled = getReactEnvVar('DIAKEM_BACKEND_API');
-
+  const dispatch = useAppDispatch();
   if (!isInstalled) {
     return <div>NOT INSTALLED</div>;
   }
@@ -30,6 +43,24 @@ export const App = () => {
     <div>
       <h1>Environment Mode: {getEnvironment()}</h1>
       <h1>API-URL: {isInstalled}</h1>
+      {!isAuthenticated && <LoginPage />}
+      <Test />
+      {/* <Button */}
+      {/*  onClick={() => */}
+      {/*    dispatch( */}
+      {/*      authSlice.actions.login({ */}
+      {/*        name: 'asd', */}
+      {/*        email: 'asd', */}
+      {/*        password: 'asd', */}
+      {/*        _id: 'asdasd', */}
+      {/*        lastLogin: 'asd', */}
+      {/*        username: 'asd', */}
+      {/*      }) */}
+      {/*    ) */}
+      {/*  } */}
+      {/* > */}
+      {/*  Login */}
+      {/* </Button> */}
     </div>
   );
 };
